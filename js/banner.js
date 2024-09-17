1b6d3df36a412e02862a7a44d65cbd5e4c2af3af
Caio Cesar <caiocesar318@GMAIL.COM>
2024-09-17 15:37:48 -0300

    Arquivos

---
+++ js/banner.js
@@ -0,0 +1,99 @@
+(function($, window, document, undefined ) {
+
+$.fn.sss = function(options) {
+
+	var settings = $.extend({
+	slideShow : true,
+	startOn : 0,
+	speed : 4000,
+	transition : 400,
+	arrows : true
+	}, options);
+
+	return this.each(function() {
+
+// Variaveis
+
+	var
+	wrapper = $(this),
+	slides = wrapper.children().wrapAll('<div class="slide"/>').addClass('ssslide'),
+	slider = wrapper.find('.slide'),
+	slide_count = slides.length,
+	transition = settings.transition,
+	starting_slide = settings.startOn,
+	target = starting_slide > slide_count - 1 ? 0 : starting_slide,
+	animating = false,
+	clicked,
+	timer,
+	key,
+	prev,
+	next,
+
+// Reset Slide
+
+	reset_timer = settings.slideShow ? function() {
+	clearTimeout(timer);
+	timer = setTimeout(next_slide, settings.speed);
+	} : $.noop;
+
+// Animação do Slide
+
+	function get_height(target) {
+	return ((slides.eq(target).height() / slider.width()) * 100) + '%';
+	}
+
+	function animate_slide(target) {
+	if (!animating) {
+	animating = true;
+	var target_slide = slides.eq(target);
+
+	target_slide.fadeIn(transition);
+	slides.not(target_slide).fadeOut(transition);
+
+	slider.animate({paddingBottom: get_height(target)}, transition,  function() {
+	animating = false;
+	});
+
+	reset_timer();
+
+	}};
+
+	function next_slide() {
+	target = target === slide_count - 1 ? 0 : target + 1;
+	animate_slide(target);
+	}
+
+	function prev_slide() {
+	target = target === 0 ? slide_count - 1 : target - 1;
+	animate_slide(target);
+	}
+
+	if (settings.arrows) {
+	slider.append('<div class="sssprev"></div>', '<div class="sssnext"></div>');
+	}
+
+	next = slider.find('.sssnext'),
+	prev = slider.find('.sssprev');
+
+	$(window).load(function() {
+
+	slider.css({paddingBottom: get_height(target)}).click(function(e) {
+	clicked = $(e.target);
+	if (clicked.is(next)) { next_slide() }
+	else if (clicked.is(prev)) { prev_slide() }
+	});
+
+	animate_slide(target);
+
+	$(document).keydown(function(e) {
+	key = e.keyCode;
+	if (key === 39) { next_slide() }
+	else if (key === 37) { prev_slide() }
+	});
+
+	});
+
+});
+
+};
+})(jQuery, window, document);
